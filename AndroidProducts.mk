c9d72b89a971552f40e146e4b4604572c7cf5f95
Kreato <kerimkocabas@tutanota.com>
2022-07-31 19:26:06 +0300

    beyondx: Adapt to fluid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyondx.mk
+    $(LOCAL_DIR)/fluid_beyondx.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyondx-eng \
-    lineage_beyondx-user \
-    lineage_beyondx-userdebug
+    fluid_beyondx-eng \
+    fluid_beyondx-user \
+    fluid_beyondx-userdebug
